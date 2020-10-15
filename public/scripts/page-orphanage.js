2316c2e8305912b63825baee7577612059fec9d5
Carlos <carlossouza_94@hotmail.com>
2020-10-15 12:30:02 -0300

    Day 2

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,51 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false,
+}
+
+//create map
+const map = L.map('mapid', options).setView([-22.2306542,-45.935184], 15)
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', ).addTo(map)
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29,68],
+    popupAnchor: [170,2]
+})
+
+
+
+//create and add marker
+L.marker([-22.2306542,-45.935184], {icon})
+.addTo(map)
+
+/* img gallery */
+
+function selectImage(event){
+    const button = event.currentTarget
+
+    //remover todas as classes .active
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button){
+        button.classList.remove("active")
+    }
+
+    //selecionar a image clicada
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+    //atualizar o container de images
+    imageContainer.src = image.src
+
+    //adicionar classe .active
+    button.classList.add('active')
+}
