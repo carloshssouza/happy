d514aa8f2cc5614924615f23f99364d0c6624d28
Carlos <carlossouza_94@hotmail.com>
2020-10-23 15:26:22 -0300

    Day 4 update

---
+++ src/server.js
@@ -0,0 +1,21 @@
+//importar depedencias
+const express = require('express');
+const path = require('path');
+
+//iniciando o express
+const server = express()
+server
+    //utilizando arquivos estáticos
+    .use(express.static('public'))
+
+    //configurar template engine
+    .set('views', path.join(__dirname, "views"))
+    .set('view engine', 'hbs')
+
+    //criar rota
+    .get('/', (req, res) => {
+        return res.render('index')
+    })
+
+//ligar o servidor
+server.listen(5500)
